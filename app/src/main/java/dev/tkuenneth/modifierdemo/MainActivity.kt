f2666c3257eaeef8c22285900cf7d19adc1c1ad5
Thomas Künneth <kuennetht@gmail.com>
2025-08-16 09:00:26 +0200

    GraphicsLayerDemo added

--- app/src/main/java/dev/tkuenneth/modifierdemo/MainActivity.kt
+++ app/src/main/java/dev/tkuenneth/modifierdemo/MainActivity.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.contextmenu.builder.item
 import androidx.compose.foundation.text.contextmenu.modifier.appendTextContextMenuComponents
@@ -33,6 +35,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.keepScreenOn
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
@@ -56,13 +59,10 @@ fun ModifierDemo() {
         colorScheme = defaultColorScheme()
     ) {
         Scaffold(
-            modifier = Modifier.fillMaxSize(),
-            topBar = {
+            modifier = Modifier.fillMaxSize(), topBar = {
                 TopAppBar(
-                    title = { Text(stringResource(R.string.app_name)) }
-                )
-            }
-        ) { innerPadding ->
+                    title = { Text(stringResource(R.string.app_name)) })
+            }) { innerPadding ->
             var magnifierActive by remember { mutableStateOf(false) }
             Column(
                 modifier = Modifier
@@ -70,12 +70,16 @@ fun ModifierDemo() {
                     .fillMaxSize()
                     .padding(innerPadding)
                     .padding(horizontal = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                MagnifierDemo(magnifierActive = magnifierActive) { magnifierActive = it }
-                KeepScreenOnDemo()
+                Column(modifier = Modifier.align(Alignment.Start)) {
+                    MagnifierDemo(magnifierActive = magnifierActive) { magnifierActive = it }
+                    KeepScreenOnDemo()
+                }
                 UriHandlerDemo()
                 ContextMenuDemo(stringResource(R.string.context_menu))
+                GraphicsLayerDemo()
             }
         }
     }
@@ -103,21 +107,22 @@ fun KeepScreenOnDemo() {
 }
 
 @Composable
-fun ColumnScope.UriHandlerDemo() {
+fun UriHandlerDemo() {
     val uriHandler = LocalUriHandler.current
     val url = stringResource(R.string.url)
     Text(
-        text = url,
-        modifier = Modifier
-            .align(alignment = Alignment.CenterHorizontally)
-            .clickable {
-                try {
-                    uriHandler.openUri(url)
-                } catch (_: IllegalArgumentException) {
-                    // Do something
-                }
+        text = url, modifier = Modifier.clickable {
+//                try {
+//                    uriHandler.openUri(url)
+//                } catch (_: IllegalArgumentException) {
+//                    // Do something
+//                }
+            runCatching {
+                uriHandler.openUri(url)
+            }.onFailure {
+                // Do something
             }
-    )
+        })
 }
 
 @Composable
@@ -155,4 +160,42 @@ fun ContextMenuDemo(text: String) {
 
 sealed class ContextMenuComponentsKey {
     data object Clear : ContextMenuComponentsKey()
+}
+
+@Composable
+fun WithReflection(
+    modifier: Modifier, content: @Composable () -> Unit
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        content()
+        Box(modifier = modifier) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun GraphicsLayerDemo() {
+    WithReflection(
+        modifier = Modifier.graphicsLayer {
+            scaleX = 1.08f
+            scaleY = -1.1f
+            alpha = 0.3f
+            rotationX = 45F
+            translationY = -14F
+        }) { TextWithMarquee() }
+}
+
+@Composable
+fun TextWithMarquee() {
+    Text(
+        text = stringResource(R.string.pangram),
+        modifier = Modifier
+            .width(200.dp)
+            .background(color = MaterialTheme.colorScheme.tertiaryContainer)
+            .basicMarquee(),
+        style = MaterialTheme.typography.headlineLarge
+    )
 }

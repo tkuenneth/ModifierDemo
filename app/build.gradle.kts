44cf15ad7c928d3e91ae8a09e10bef2b1f539da0
Thomas Künneth <kuennetht@gmail.com>
2026-08-08 12:04:26 +0200

    Libraries and tools upgrades

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,16 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
 
 android {
     namespace = "dev.tkuenneth.modifierdemo"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "dev.tkuenneth.modifierdemo"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
